efc6468dabaff323639b847afd9ba7c070a14f48
Abila Khan <khankeya961@gmail.com>
2026-07-19 13:35:54 +0600

    Update CartPage.jsx
    update

--- Frontend/src/Pages/CartPage.jsx
+++ Frontend/src/Pages/CartPage.jsx
@@ -1,19 +1,42 @@
+// import Navbar from "../Components/Navbar";
+// import Cart from "../Components/Cart";
+
+
+// function CartPage({ cart, setCart,orders , setOrders}) {
+
+//     return(
+//         <>
+//             <Navbar cart={cart}/>
+//             <Cart
+//                 cart={cart}
+//                 setCart={setCart}
+//                 orders={orders}
+//                 setOrders={setOrders}
+//             />
+//         </>
+//     );
+// }
+
+// export default CartPage;
+
+
 import Navbar from "../Components/Navbar";
 import Cart from "../Components/Cart";
+import "./CartPage.css";
 
-function CartPage({ cart, setCart,orders , setOrders}) {
+function CartPage({ cart, setCart, orders, setOrders }) {
+  return (
+    <div className="cart-page">
+      <Navbar cart={cart} />
 
-    return(
-        <>
-            <Navbar cart={cart}/>
-            <Cart
-                cart={cart}
-                setCart={setCart}
-                orders={orders}
-                setOrders={setOrders}
-            />
-        </>
-    );
+      <Cart
+        cart={cart}
+        setCart={setCart}
+        orders={orders}
+        setOrders={setOrders}
+      />
+    </div>
+  );
 }
 
 export default CartPage;
